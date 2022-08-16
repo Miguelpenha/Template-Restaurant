0258e6c31d60bab49117f90e84d2938f6575b3c0
Miguel Penha <miguel.penha1@gmail.com>
2022-08-16 05:10:25 -0300

    Ajeitando algumas coiss

--- pages/Home/index.tsx
+++ pages/Home/index.tsx
@@ -3,12 +3,13 @@ import { useState, useCallback, useEffect } from 'react'
 import { useTheme } from 'styled-components'
 import { useFocusEffect} from '@react-navigation/native'
 import ContainerPd from '../../components/ContainerPd'
-import { FlatList, ListRenderItemInfo, RefreshControl } from 'react-native'
+import { FlatList, ListRenderItemInfo, RefreshControl, Platform } from 'react-native'
 import { IPlate } from '../../types'
 import Header from './Header'
 import Plate from './Plate'
 import api from '../../api'
 import { RFPercentage } from 'react-native-responsive-fontsize'
+import { Loading } from './style'
 
 export default function Home() {
   const { list } = useList()
@@ -47,27 +48,31 @@ export default function Home() {
   }
 
   useFocusEffect(useCallback(() => makeBalance(), [list]))
-  
-  return (
-    <ContainerPd scroll={false}>
-      <FlatList
-        data={plates}
-        keyExtractor={(item: IPlate) => item._id}
-        contentContainerStyle={{ paddingBottom: '10%' }}
-        ListHeaderComponent={<Header plates={plates} balance={balance} find={find} setFind={setFind}/>}
-        renderItem={({ item }: ListRenderItemInfo<IPlate>) => (
-          (item.name.toUpperCase().includes(find.toUpperCase()) || item.description.toUpperCase().includes(find.toUpperCase())) && <Plate plate={item}/>
-        )}
-        refreshControl={(
-          <RefreshControl
-            refreshing={refreshing}
-            colors={[theme.primary]}
-            onRefresh={onRefreshAction}
-            progressViewOffset={RFPercentage(11.5)}
-            progressBackgroundColor={theme.secondary}
-          />
-        )}
-      />
-    </ContainerPd>
-  )
+
+  if (plates) {
+    return (
+      <ContainerPd scroll={false}>
+        <FlatList
+          data={plates}
+          keyExtractor={(item: IPlate) => item._id}
+          contentContainerStyle={{ paddingBottom: '10%' }}
+          ListHeaderComponent={<Header plates={plates} balance={balance} find={find} setFind={setFind}/>}
+          renderItem={({ item }: ListRenderItemInfo<IPlate>) => (
+            (item.name.toUpperCase().includes(find.toUpperCase()) || item.description.toUpperCase().includes(find.toUpperCase())) && <Plate plate={item}/>
+          )}
+          refreshControl={(
+            <RefreshControl
+              refreshing={refreshing}
+              colors={[theme.primary]}
+              onRefresh={onRefreshAction}
+              progressViewOffset={RFPercentage(11.5)}
+              progressBackgroundColor={theme.secondary}
+            />
+          )}
+        />
+      </ContainerPd>
+    )
+  } else {
+    return <Loading color={theme.primary} size={Platform.OS === 'android' ? 50 : 'large'}/>
+  }
 }
